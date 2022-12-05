a58af5e396d5393196550d75ce027eb0b53f7ac6
nawat <n.ngerncham@gmail.com>
2022-12-05 22:50:58 +0700

    feat: simple navbar added

--- icabroad/icabroad/routes/routes.py
+++ icabroad/icabroad/routes/routes.py
@@ -1,7 +1,21 @@
 from flask import render_template
 from flask import current_app as app
+from .routeutils import NavbarLink
 
 
-@app.route("/")
-def home():
-    return render_template("home.html")
+@app.route("/", methods=["GET"])
+def index():
+    links = [
+        NavbarLink("Partner Universities", "/partners"),
+        NavbarLink("Buddy Program", "/partners"),
+        NavbarLink("Apply", "/apply"),
+        NavbarLink("Scholarships", "/scholarships"),
+        NavbarLink("Course Equivalency", "/course-equiv"),
+        NavbarLink("FAQ", "/faq"),
+    ]
+    return render_template("base.html", links=links)
+
+
+@app.route("/kanat", methods=["GET"])
+def kanat():
+    return render_template("kanat.html")
