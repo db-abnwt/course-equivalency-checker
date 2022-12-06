0d29c3b3571a590cab6e8a22eb4c72103b38f5b8
danamestrial <chayapolbun@gmail.com>
2022-12-06 18:19:10 +0700

    feat: Login and Register Page

--- icabroad/icabroad/routes/routes.py
+++ icabroad/icabroad/routes/routes.py
@@ -1,9 +1,10 @@
-from flask import render_template, redirect
+from flask import render_template, redirect, request, url_for
 from flask import current_app as app
 from flaskext.mysql import MySQL
 from .routeutils import NavbarLink
+from werkzeug.security import generate_password_hash, check_password_hash
 
-mysql = MySQL()
+mysql = MySQL(app)
 
 links = [
     NavbarLink("Partner Universities", "/partners"),
@@ -32,3 +33,53 @@ def home():
 @app.route("/aj-kanat", methods=["GET"])
 def aj_kanat():
     return render_template("kanat.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    elif request.method == 'POST':
+        loginDetails = request.form
+        id = loginDetails['username']
+        password = loginDetails['password']
+        with mysql.connect().cursor() as cur:
+            try:
+                query = f"select stu_id,password from users where stu_id = {id}"
+                cur.execute(query)
+                result = cur.fetchall()
+                if check_password_hash(result[0][1], password):
+                    return render_template("login.html", error="Success")
+                return render_template("login.html", error="Wrong password")
+            except:
+                return render_template("login.html", error = "Student_ID doesn't not exist [1]")
+    return render_template("login.html")
+
+@app.route('/register', methods=["GET", "POST"])
+def register():
+    if request.method == 'GET':
+        return render_template('register.html')
+    if request.method == 'POST':
+        registerDetails = request.form
+        id = int(registerDetails['username'])
+        password = registerDetails['password']
+        cpassword = registerDetails['confirm_password']
+        
+        if password != cpassword:
+            return render_template('register.html', error='Password is different')
+
+        hashed_pw = generate_password_hash(password)
+
+        with mysql.connect().cursor() as cur:
+            try:
+                query = (
+                    f"INSERT INTO "
+                    f"users(stu_id,password) "
+                    f"VALUES({id}, '{hashed_pw}')"
+                )
+                cur.execute(query)
+                cur.connection.commit()
+                return render_template('register.html', error='Register Succesful, Redirecting to Login Page')\
+                , {"Refresh": "3; url=/login"}
+            except:
+                return render_template('register.html', error='Kaboom')
+    return render_template('register.html')
