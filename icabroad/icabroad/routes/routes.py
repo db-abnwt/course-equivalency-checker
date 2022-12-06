7e8c6d85e9908074725cc82f53119e3766f0c5c9
nawat <n.ngerncham@gmail.com>
2022-12-06 21:36:59 +0700

    feat: continents page added

--- icabroad/icabroad/routes/routes.py
+++ icabroad/icabroad/routes/routes.py
@@ -3,18 +3,9 @@ from flask import render_template, redirect, request
 from flaskext.mysql import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from .routeutils import NavbarLink, ContinentLink
+from .routeutils import get_all_continents
 
 mysql = MySQL(app)
-
-links = [
-    NavbarLink("Partner Universities", "/partners"),
-    NavbarLink("Buddy Program", "/buddy"),
-    NavbarLink("Apply", "/apply"),
-    NavbarLink("Scholarships", "/scholarships"),
-    NavbarLink("Course Equivalency", "/course-equiv"),
-    NavbarLink("FAQ", "/faq"),
-]
 
 
 @app.route("/", methods=["GET"])
@@ -24,13 +15,12 @@ def root():
 
 @app.route("/home", methods=["GET"])
 def home():
-    with mysql.connect().cursor() as cur:
-        def generate_continent(continent: tuple[str]): return ContinentLink(continent)
+    return render_template("home.html", continents=get_all_continents())
 
-        query = "select distinct continent from country;"
-        cur.execute(query)
-        continents = map(generate_continent, cur.fetchall())
-        return render_template("home.html", links=links, continents=continents)
+
+@app.route("/continent", methods=["GET"])
+def continent():
+    return render_template("continent.html", continents=get_all_continents())
 
 
 @app.route("/aj-kanat", methods=["GET"])
--- icabroad/icabroad/routes/routeutils.py
+++ icabroad/icabroad/routes/routeutils.py
@@ -1,13 +1,21 @@
-from dataclasses import dataclass
+from flaskext.mysql import MySQL
+from flask import current_app as app
 
-
-@dataclass
-class NavbarLink:
-    name: str
-    url: str
+mysql = MySQL(app)
 
 
 class ContinentLink:
     def __init__(self, continent: tuple[str]):
         self.normal, = continent
         self.simple = self.normal.lower().replace(" ", "")
+
+    @staticmethod
+    def generate_continent(continent_tup: tuple[str]):
+        return ContinentLink(continent_tup)
+
+
+def get_all_continents() -> list[ContinentLink]:
+    with mysql.connect().cursor() as cur:
+        query = "select distinct continent from country;"
+        cur.execute(query)
+        return list(map(ContinentLink.generate_continent, cur.fetchall()))
